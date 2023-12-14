f6bc30496fe13a7f621ab472764a82ccdb758f55
emilio <124540296+EmilioMarquez137@users.noreply.github.com>
2023-12-13 20:03:06 -0600

    c1

--- segmentacion.py
+++ segmentacion.py
@@ -1,1 +1,12 @@
+import cv2
+import os
+import numpy as np
 
+rutas = ["videoSegmentation/vid1.mp4","videoSegmentation/vid2.mp4","videoSegmentation/vid3.mp4"]
+idRutas = 1
+for i in rutas:
+    vidcap = cv2.VideoCapture(i)
+    success,currentImage = vidcap.read()
+    prevImage = currentImage
+    print(idRutas)
+    count = 0
