697a919172956271ad27cc79817ef4c2fb5b48cb
lamusaliza <lizaliza7770@gmail.com>
2023-12-13 20:14:57 -0600

    prcent_cambio

--- segmentacion.py
+++ segmentacion.py
@@ -10,3 +10,11 @@ for i in rutas:
     prevImage = currentImage
     print(idRutas)
     count = 0
+
+    total_pixeles = frame_diff.size
+    porcentaje_cambio = (pixeles_cambiados / total_pixeles) * 100
+
+    print(f'Porcentaje de cambio: {porcentaje_cambio:.2f}%')
+
+    kernel = np.ones((2, 2), np.uint8)
+    erodeImage = cv2.erode(binFrame, kernel, iterations = 1)
